813dd4ec424428a8af5135e5447fe8559fc6f93c
NTKhang <21520963@gm.uit.edu.vn>
2023-07-04 16:15:04 +0700

    homework-lesson1

--- exercises/basic-of-rust/src/conditions.rs
+++ exercises/basic-of-rust/src/conditions.rs
@@ -4,15 +4,25 @@
 // - another function call
 // - additional variables
 pub fn bigger(a: i32, b: i32) -> i32 {
-    todo!()
+    if a > b {
+        a
+    } else {
+        b
+    }
 }
 
 //Exercise 2
 // Input: Provide an arbitrary value of number
 // Check number is Positive or Negative or Zero
 // Output: &str
-fn check_number(number: u32) -> &'static str {
-    todo!()
+fn check_number(number: i32) -> &'static str {
+    if number < 0 {
+        return "Negative"
+    } else if number > 0 {
+        return "Positive"
+    } else {
+        return "Zero"
+    }
 }
 
 // Exercise 3
@@ -20,10 +30,10 @@ fn check_number(number: u32) -> &'static str {
 // Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
 
 pub fn foo_if_fizz(fizzish: &str) -> &str {
-    if fizzish == "fizz" {
-        "foo"
-    } else {
-        1
+    match fizzish {
+        "fizz" => return "foo",
+        "fuzz" => return "bar",
+        _ => return "baz",
     }
 }
 
@@ -31,14 +41,21 @@ pub fn foo_if_fizz(fizzish: &str) -> &str {
 // Determine if a given year is a leap year
 // Implement logic
 fn is_leap_year(year: i32) -> bool {
-    todo!()
+    if (year % 100 != 0 && year % 4 == 0) | (year % 400 == 0) {
+        return true
+    }
+    false
 }
 
 // Exercise 5
 // Calculate the factorial of a number
 // Implement logic
 fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut temp = 1;
+    for i in 1..n+1 {
+        temp = temp * i;
+    }
+    temp
 }
 
 // Exercise 6
@@ -46,7 +63,19 @@ fn factorial(n: u32) -> u32 {
 // Implement logic
 
 fn is_prime(n: u32) -> bool {
-    todo!()
+    if (n == 0) | (n == 1) {
+        return false;
+    }
+    let mut temp = 1;
+    for i in 2..n/2+1 {
+        if n % i == 0 {
+            temp = temp + 1;
+        }
+    }
+    if temp == 1 {
+        return true;
+    }
+    false
 }
 
 
